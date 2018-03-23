45c2a3f9b8ace54344bc1989e6ecb672aaec92d4
Doug Rogers <doug.16.rogers@gmail.com>
2018-03-23 02:32:59 -0400

    Add Jeff's get_line.adb that works for mixed line-endings.

--- get_line.adb
+++ get_line.adb
@@ -2,14 +2,33 @@ with Ada.Text_IO;
 
 use Ada;
 function Get_Line return String is
-   Line : String (1 .. 256);
-   Last : Natural;
+   Line     : String (1 .. 256);
+   Last     : Natural;
+   I        : Positive := 1;
+   Old_Last : Natural;
+
+   function Is_Legal (C : in Character) return Boolean is
+   begin -- Is_Legal
+      return C in 'A' .. 'Z' or C in 'a' .. 'z' or C in '1' .. '9';
+   end Is_Legal;
 begin -- Get_Line
    Text_IO.Get_Line (Item => Line, Last => Last);
+   Old_Last := Last;
 
-   if Last < Line'Last then
+   Remove_Illegal_Characters : loop
+      exit Remove_Illegal_Characters when I > Last;
+
+      if Is_Legal (Line (I) ) then
+         I := I + 1;
+      else
+         Last := Last - 1;
+         Line (I .. Last) := Line (I + 1 .. Last + 1);
+      end if;
+   end loop Remove_Illegal_Characters;
+
+   if Old_Last < Line'Last then
       return Line (1 .. Last);
    else
-      return Line & Get_Line;
+      return Line (1 .. Last) & Get_Line;
    end if;
 end Get_Line;
